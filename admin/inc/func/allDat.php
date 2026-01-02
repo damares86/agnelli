b766993207f8877fe0ec67a7e0106032d7601744
damares86 <davidemasera@gmail.com>
2026-01-02 16:02:39 +0100

    aggiunta vista di dettaglio

--- admin/inc/func/allDat.php
+++ admin/inc/func/allDat.php
@@ -83,6 +83,7 @@ $mesi = [
           </thead>
           <tbody>
             <?php
+            $num = 0;
             foreach ($risultati as $table => $righe) {
 
               // split nome tabella
@@ -104,7 +105,7 @@ $mesi = [
                 // intercetto SALESIANI
                 if ($riga['categoria'] === 'SALESIANI') {
                   $caffeSalesiani += $riga['totale_dat'];
-                   // echo '++++++++++ caffè salesiani => '.$riga['totale_dat'].'<br>';
+                  // echo '++++++++++ caffè salesiani => '.$riga['totale_dat'].'<br>';
                 }
                 $totaleNetto +=
                   ($riga['prezzo_pubblico'] - $riga['prezzo_dat'])
@@ -113,14 +114,14 @@ $mesi = [
               }
 
               //echo 'Importo totale  => ' . $importoSenzaIva . '<br>';
-              
+
               // tolgo il caffè dei salesiani e arrotondo
               $totaleNetto -= $caffeSalesiani;
               $totaleNetto = round($totaleNetto, 2);
 
               // calcolo l'importo ivato
               $totaleIvato = round($totaleNetto * 1.22, 2);
-              
+
               //echo 'Importo da fatturare => ' . $importoDaFatturare;
 
             ?>
@@ -133,43 +134,102 @@ $mesi = [
                   <?= number_format($totaleIvato, 2, ',', '.') ?>
                 </td>
                 <td>
-                  <a href="index.php?p=editAccount&idToMod=<?= $row['id'] ?>" class="btn icon btn-success shadow edit-link" data-base-url="index.php?p=editAccount&idToMod=<?= $row['id'] ?>">
+                  <button type="button" class="btn icon btn-success shadow edit-link" data-bs-toggle="modal" data-bs-target="#detail<?= $num ?>">
                     <i class="bi bi-search"></i>
-                  </a>
+                  </button>
 
                   &nbsp; &nbsp;
-                  <a href="#" class="btn icon btn-danger shadow" data-bs-toggle="modal" data-bs-target="#danger<?= $row['id'] ?>"><i class="bi bi-trash"></i>
+                  <!--<a href="#" class="btn icon btn-danger shadow" data-bs-toggle="modal" data-bs-target="#danger<?= $row['id'] ?>"><i class="bi bi-trash"></i>-->
                   </a>
-                  <!--Danger theme Modal -->
-                  <div class="modal fade text-left" id="danger<?= $row['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel120" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+
+                  <div class="modal fade text-left w-100" id="detail<?= $num ?>" tabindex="-1" aria-labelledby="myModalLabel20" style="display: none;" aria-modal="true" role="dialog">
+                    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-full" role="document">
                       <div class="modal-content">
-                        <div class="modal-header bg-danger">
-                          <h5 class="modal-title white" id="myModalLabel120">
-                            <?= $common_modal_title_sure ?>
-                          </h5>
+                        <div class="modal-header">
+                          <h4 class="modal-title" id="myModalLabel20">Dati fatturazione Dat Caffè (<?= $nomeMeseInizio ?> - <?= $nomeMeseFine ?> <?= $anno ?>)</h4>
                           <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                             <i data-feather="x"></i>
                           </button>
                         </div>
                         <div class="modal-body">
-                          Se continui eliminerai questa fattura definitivamente.
+
+                          <div class="table-responsive">
+                            <table class="table" id="table">
+                              <thead>
+                                <tr>
+                                  <th>Categoria</th>
+                                  <th>Tab.</th>
+                                  <th>Fascia</th>
+                                  <th>Quantità</th>
+                                  <th>Prezzo DAT</th>
+                                  <th>Prezzo Pubblico</th>
+                                  <th>Totale Netto</th>
+                                  <th>Totale Ivato</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <?php
+                                $subTotaleNetto = 0;
+                                $subTotaleIvato = 0;
+
+                                foreach ($righe as $riga) {
+
+                                  // calcoli coerenti (no arrotondamenti intermedi)
+                                  $totaleNettoRiga =
+                                    ($riga['prezzo_pubblico'] - $riga['prezzo_dat'])
+                                    / 1.10
+                                    * $riga['quantita'];
+
+                                  $totaleIvatoRiga = $totaleNettoRiga * 1.22;
+
+                                  // accumulo (escludo SALESIANI se vuoi)
+                                  if ($riga['categoria'] !== 'SALESIANI') {
+                                    $subTotaleNetto += $totaleNettoRiga;
+                                    $subTotaleIvato += $totaleIvatoRiga;
+                                  }
+                                ?>
+                                  <tr>
+                                    <td><?= htmlspecialchars($riga['categoria']) ?></td>
+                                    <td><?= $riga['tabella'] ?></td>
+                                    <td><?= $riga['fascia'] ?></td>
+                                    <td><?= $riga['quantita'] ?></td>
+                                    <td><?= number_format($riga['prezzo_dat'], 2, ',', '.') ?></td>
+                                    <td><?= number_format($riga['prezzo_pubblico'], 2, ',', '.') ?></td>
+                                    <td><?= number_format($totaleNettoRiga, 2, ',', '.') ?></td>
+                                    <td><?= number_format($totaleIvatoRiga, 2, ',', '.') ?></td>
+                                  </tr>
+                                <?php } ?>
+                              </tbody>
+                              <tfoot>
+                                <tr class="fw-bold">
+                                  <td colspan="6" class="text-end">Totale</td>
+                                  <td><?= number_format($subTotaleNetto, 2, ',', '.') ?> €</td>
+                                  <td><?= number_format($subTotaleIvato, 2, ',', '.') ?> €</td>
+                                </tr>
+                              </tfoot>
+                            </table>
+                          </div>
+
+
+
+
                         </div>
                         <div class="modal-footer">
                           <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
                             <i class="bx bx-x d-block d-sm-none"></i>
-                            <span class="d-none d-sm-block"><?= $common_modal_cancel ?></span>
+                            <span class="d-none d-sm-block">Chiudi</span>
                           </button>
-                          <span class="d-none d-sm-block"><a href="core/mngAccounts.php?idToDel=<?= $row['id'] ?>" class="btn btn-danger ml-1">
-                              <?= $common_modal_confirm ?>
-                            </a></span>
+
                         </div>
                       </div>
                     </div>
                   </div>
+
+
                 </td>
               </tr>
             <?php
+              $num++;
             }
             ?>
 
