2ad8df09c1a1b90e094bfc316c95959ed8866ea3
damares86 <davidemasera@gmail.com>
2026-04-02 15:57:00 +0200

    xxx

--- index.php
+++ index.php
@@ -11,259 +11,41 @@ require 'inc/sidebar.php';
     <div class="pt-3"></div>
 
     <div class="container">
-      <div class="card mb-3">
-        <div class="card-body">
-          <div class="row g-3">
-            <div class="col-4">
-              <div class="feature-card mx-auto text-center">
-                <div class="card mx-auto bg-gray">
-                  <i class="ti ti-circle-square"></i>
-                </div>
-                <h6 class="mb-0 fz-14">PWA Support</h6>
+      <a href="rubrica.php">
+        <div class="card service-card bg-danger bg-gradient mb-3">
+          <div class="card-body">
+            <div class="d-flex gap-3 align-items-center justify-content-between">
+              <div class="service-text">
+                <h5>Rubrica</h5>
+                <!--<p class="mb-0">The write less, do more with JavaScript Library.</p>-->
               </div>
-            </div>
-
-            <div class="col-4">
-              <div class="feature-card mx-auto text-center">
-                <div class="card mx-auto bg-gray">
-                  <i class="ti ti-brand-bootstrap"></i>
-                </div>
-                <h6 class="mb-0 fz-14">Bootstrap 5.3</h6>
-              </div>
-            </div>
-
-            <div class="col-4">
-              <div class="feature-card mx-auto text-center">
-                <div class="card mx-auto bg-gray">
-                  <i class="ti ti-file-type-js"></i>
-                </div>
-                <h6 class="mb-0 fz-14">Vanilla JavaScript</h6>
+              <div class="service-img">
+                <img src="assets/img/rubrica.png" alt="">
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </a>
 
-    <div class="container">
-      <div class="card card-bg-img bg-img bg-overlay mb-3" style="background-image: url('assets/img/bg-img/3.jpg')">
-        <div class="card-body p-4">
-          <h2 class="text-white">Ready to Use Elements</h2>
-          <p class="mb-3 text-white">Unlock a collection of over 220 modern design elements. Simply copy and paste to
-            enhance your page instantly.</p>
-          <a class="btn btn-warning" href="elements.html">Explore Elements <i class="ti ti-arrow-right"></i></a>
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="card mb-3">
-        <div class="card-body">
-          <div class="row g-3">
-            <div class="col-4">
-              <div class="feature-card mx-auto text-center">
-                <div class="card mx-auto bg-gray">
-                  <i class="ti ti-brand-sass"></i>
-                </div>
-                <h6 class="mb-0 fz-14">SCSS</h6>
+      <a href="portal/manual.php?prod=1">
+        <div class="card service-card bg-info bg-gradient mb-3">
+          <div class="card-body">
+            <div class="d-flex gap-3 align-items-center justify-content-between">
+              <div class="service-text">
+                <h5>Guida</h5>
               </div>
-            </div>
-
-            <div class="col-4">
-              <div class="feature-card mx-auto text-center">
-                <div class="card mx-auto bg-gray">
-                  <i class="ti ti-brand-npm"></i>
-                </div>
-                <h6 class="mb-0 fz-14">npm</h6>
-              </div>
-            </div>
-
-            <div class="col-4">
-              <div class="feature-card mx-auto text-center">
-                <div class="card mx-auto bg-gray">
-                  <i class="ti ti-bulb"></i>
-                </div>
-                <h6 class="mb-0 fz-14">Gulp 4</h6>
+              <div class="service-img">
+                <img src="assets/img/white-question-mark.svg" alt="">
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </a>
+
+      <div class="pb-3"></div>
     </div>
 
-    <div class="container">
-      <div class="card bg-primary mb-3 bg-img" style="background-image: url('assets/img/core-img/1.png')">
-        <div class="card-body p-4">
-          <h2 class="text-white">35+ Pre-Built Pages</h2>
-          <p class="mb-3 text-white">A collection of 35+ fully designed pages, including Authentication, Chats,
-            eCommerce, Blogs, and more—ready to use instantly.</p>
-          <a class="btn btn-warning" href="pages.html">Browse All Pages <i class="ti ti-arrow-right"></i></a>
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="card mb-3">
-        <div class="card-body">
-          <div class="row g-3">
-            <div class="col-4">
-              <div class="feature-card mx-auto text-center">
-                <div class="card mx-auto bg-gray">
-                  <i class="ti ti-sun-moon"></i>
-                </div>
-                <h6 class="mb-0 fz-14">Dark Mode</h6>
-              </div>
-            </div>
-
-            <div class="col-4">
-              <div class="feature-card mx-auto text-center">
-                <div class="card mx-auto bg-gray">
-                  <i class="ti ti-text-direction-rtl"></i>
-                </div>
-                <h6 class="mb-0 fz-14">RTL Support</h6>
-              </div>
-            </div>
-
-            <div class="col-4">
-              <div class="feature-card mx-auto text-center">
-                <div class="card mx-auto bg-gray">
-                  <i class="ti ti-code"></i>
-                </div>
-                <h6 class="mb-0 fz-14">Clean Code</h6>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="card bg-secondary mb-3">
-        <div class="card-body">
-          <h3>Customer Review</h3>
-
-          <div class="testimonial-slide-three-wrapper" dir="ltr">
-            <div class="testimonial-slide3 testimonial-style3">
-
-              <!-- Single Testimonial Slide -->
-              <div class="single-testimonial-slide">
-                <div class="text-content">
-                  <div class="d-flex gap-1 mb-2">
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                  </div>
-                  <h6 class="mb-2">The code looks clean, and the designs are excellent. I recommend.</h6>
-                  <span class="d-block">Mrrickez, Themeforest</span>
-                </div>
-              </div>
-
-              <!-- Single Testimonial Slide -->
-              <div class="single-testimonial-slide">
-                <div class="text-content">
-                  <div class="d-flex gap-1 mb-2">
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                  </div>
-                  <h6 class="mb-2">All complete, <br> great craft.</h6>
-                  <span class="d-block">Mazatlumm, Themeforest</span>
-                </div>
-              </div>
-
-              <!-- Single Testimonial Slide -->
-              <div class="single-testimonial-slide">
-                <div class="text-content">
-                  <div class="d-flex gap-1 mb-2">
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                  </div>
-                  <h6 class="mb-2">Awesome template! <br> Excellent support!</h6>
-                  <span class="d-block">Vguntars, Themeforest</span>
-                </div>
-              </div>
-
-              <!-- Single Testimonial Slide -->
-              <div class="single-testimonial-slide">
-                <div class="text-content">
-                  <div class="d-flex gap-1 mb-2">
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                  </div>
-                  <h6 class="mb-2">Nice modern design, I love the product.</h6>
-                  <span class="d-block">electroMEZ, Themeforest</span>
-                </div>
-              </div>
-
-              <!-- Single Testimonial Slide -->
-              <div class="single-testimonial-slide">
-                <div class="text-content">
-                  <div class="d-flex gap-1 mb-2">
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                    <i class="ti ti-star-fill text-warning"></i>
-                  </div>
-                  <h6 class="mb-2">Excquisite pixel perfect design, with an abundance of content, well documented.</h6>
-                  <span class="d-block">webpixie, Themeforest</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="card">
-        <div class="card-body">
-          <div class="row g-3">
-            <div class="col-4">
-              <div class="feature-card mx-auto text-center">
-                <div class="card mx-auto bg-gray">
-                  <i class="ti ti-carambola"></i>
-                </div>
-                <h6 class="mb-0 fz-14">Top Rated</h6>
-              </div>
-            </div>
-
-            <div class="col-4">
-              <div class="feature-card mx-auto text-center">
-                <div class="card mx-auto bg-gray">
-                  <i class="ti ti-award"></i>
-                </div>
-                <h6 class="mb-0 fz-14">Elegant Design</h6>
-              </div>
-            </div>
-
-            <div class="col-4">
-              <div class="feature-card mx-auto text-center">
-                <div class="card mx-auto bg-gray">
-                  <i class="ti ti-flame"></i>
-                </div>
-                <h6 class="mb-0 fz-14">Innovative & Trendy</h6>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="pb-3"></div>
-  </div>
-
-  <?php
-  require 'inc/nav.php';
-  require 'inc/footer.php';
-  ?>
+    <?php
+    require 'inc/nav.php';
+    require 'inc/footer.php';
+    ?>
