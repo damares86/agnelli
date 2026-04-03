1057b607b39da1345722ee262896d33263b9193b
damares86 <davidemasera@gmail.com>
2026-04-03 11:18:26 +0200

    sviluppo completato, si passa alla gestione online

--- inc/header.php
+++ inc/header.php
@@ -114,7 +114,6 @@ foreach (glob("admin/locale/$lang/*.php") as $row) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-  <meta name="theme-color" content="#0134d4">
   <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
--- inc/sidebar.php
+++ inc/sidebar.php
@@ -10,90 +10,46 @@
       <div class="sidenav-wrapper">
         <!-- Sidenav Profile -->
         <div class="sidenav-profile bg-gradient">
-          <div class="sidenav-style1"></div>
 
           <!-- User Thumbnail -->
           <div class="user-profile">
-            <img src="assets/img/bg-img/2.jpg" alt="">
+            <img src="admin/uploads/avatar/<?= $_SESSION['avatar'] ?>" alt="">
           </div>
 
           <!-- User Info -->
           <div class="user-info">
-            <h6 class="user-name mb-0">Affan Islam</h6>
-            <span>CEO, Designing World</span>
+            <h6 class="user-name mb-0"><?= $_SESSION['username'] ?></h6>
+            <span><?= $_SESSION['rolename'] ?></span>
           </div>
         </div>
 
         <!-- Sidenav Nav -->
         <ul class="sidenav-nav ps-0">
           <li>
-            <a href="home.html"><i class="ti ti-smart-home"></i> Home</a>
+            <a href="index.php"><i class="ti ti-smart-home"></i> Home</a>
           </li>
           <li>
-            <a href="elements.html"><i class="ti ti-heart"></i> Elements
-              <span class="badge bg-danger rounded-pill">220+</span>
+            <a href="rubrica.php"><i class="ti ti-address-book"></i> Rubrica
             </a>
           </li>
           <li>
-            <a href="pages.html"><i class="ti ti-folders"></i> Pages
-              <span class="badge bg-success rounded-pill">100+</span>
+            <a href="admin"><i class="ti ti-settings"></i> Admin area
             </a>
           </li>
           <li>
-            <a href="#"><i class="ti ti-shopping-bag"></i> Shop</a>
-            <ul>
-              <li>
-                <a href="shop-grid.html"> Shop Grid</a>
-              </li>
-              <li>
-                <a href="shop-list.html"> Shop List</a>
-              </li>
-              <li>
-                <a href="shop-details.html"> Shop Details</a>
-              </li>
-              <li>
-                <a href="cart.html"> Cart</a>
-              </li>
-              <li>
-                <a href="checkout.html"> Checkout</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="settings.html"><i class="ti ti-settings"></i> Settings</a>
-          </li>
-          <li>
-            <div class="night-mode-nav">
-              <i class="ti ti-moon"></i> Night Mode
-              <div class="form-check form-switch">
-                <input class="form-check-input form-check-success" id="darkSwitch" type="checkbox">
-              </div>
-            </div>
-          </li>
-          <li>
-            <a href="login.html"><i class="ti ti-logout"></i> Logout</a>
+            <a href="admin/core/logout.php"><i class="ti ti-logout"></i> Logout</a>
           </li>
         </ul>
 
-        <div class="mt-auto">
-          <!-- Social Info -->
-          <div class="social-info-wrap">
-            <a href="#">
-              <i class="ti ti-brand-facebook"></i>
-            </a>
-            <a href="#">
-              <i class="ti ti-brand-x"></i>
-            </a>
-            <a href="#">
-              <i class="ti ti-brand-linkedin"></i>
-            </a>
-          </div>
 
           <!-- Copyright Info -->
           <div class="copyright-info">
             <p>
-              <span id="copyrightYear"></span>
-              &copy; Made by <a href="#"> Designing World</a>
+              <img src="admin/assets/images/logo/damares_rid.png"> damares v.<?= $damares_version?>
+            </p>
+            <p class="pt-4">
+              <!--<span id="copyrightYear"></span>-->
+              Developed by <a href="https://dmweblab.com/" target="_blank"><img src="admin/assets/images/logo/dmweblab_logo.png"></a>
             </p>
           </div>
         </div>
